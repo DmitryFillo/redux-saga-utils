57b77da6c81f25f4a9fcc78ba53ac1ee72469eb4
Dmitry Fillo <fillo@fillo.me>
2017-03-26 23:02:39 +0300

    change builder options, dependecies and make output more optimized

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,20 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'ReduxSagaUtils',
-        libraryTarget: 'umd',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-            }
-        ]
-    },
-
-    // All from the peer/prod dependencies should be there.
-    externals : {
-        'redux-saga/effects': {
-            commonjs: "redux-saga/effects",
-            commonjs2: "redux-saga/effects",
-            amd: "redux-saga/effects",
-            root: "ReduxSaga.effects",
-        },
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  // All from the peer/prod dependencies should be there.
+  externals: ['redux-saga/effects', 'redux-saga', /^babel-runtime/],
 };
